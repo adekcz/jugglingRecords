6b845b76f6a14978b579cd9086dba11887405960
adekcz <adekcz@gmail.com>
2016-12-23 00:32:00 +0100

    admin page

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+myModels = [Record, UserProfile, RecordCategory]  # iterable list
+
+admin.site.register(myModels)
