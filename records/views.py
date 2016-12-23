0fbc186977cd36837ecc0e45062f69e50b17b386
adekcz <adekcz@gmail.com>
2016-12-23 01:58:00 +0100

    view for Profile Page

--- records/views.py
+++ records/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import UserProfile, Record
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def profilePage(request, param):
+    print(param)
+    print("\n'n'nAAA\n\n\n")
+    user1 = UserProfile.objects.all()[0]
+    records = Record.objects.filter(user__username=param)
+
+    template = loader.get_template("records/profilePage.html")
+    context = { 
+            "userProfile": user1, 
+            "records": records }
+
+    return HttpResponse(template.render(context, request))
