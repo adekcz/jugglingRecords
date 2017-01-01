51536aad64209bc3d1df3bdfb786a5d8c47270a2
adekcz <adekcz@gmail.com>
2017-01-01 20:16:08 +0100

    various stylistic changes popup menu in header filters visully collapsed

--- records/forms.py
+++ records/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from django import forms
 from .models import Record
 
 class RegisterForm(ModelForm):
@@ -11,3 +12,8 @@ class NewRecordForm(ModelForm):
     class Meta:
         model = Record
         fields = ('public','category', 'catches', 'endurance_time', 'record_happened', 'url_to_proof' )
+
+class ProfileForm(forms.Form):
+    first_name = forms.CharField(label='Your name', max_length=100)
+    last_name = forms.CharField(label='Your last name', max_length=100)
+    pass
