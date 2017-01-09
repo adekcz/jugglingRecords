ce644cd5ef651113a86778552035ab5bb8f546ec
adekcz <adekcz@gmail.com>
2017-01-09 01:47:46 +0100

    Hack fixed 'Oops! improperly configured forms' error

--- records/static/records/js/webflow.js
+++ records/static/records/js/webflow.js
@@ -1230,7 +1230,7 @@
     var chimpRegex = /list-manage[1-9]?.com/i;
 
     var disconnected = _.debounce(function() {
-      alert('Oops! This page has improperly configured forms. Please contact your website administrator to fix this issue.');
+      //alert('Oops! This page has improperly configured forms. Please contact your website administrator to fix this issue.');
     }, 100);
 
     api.ready = function() {
